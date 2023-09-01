ca955285b287349b3c8f6a2013e621ee097f358c
Andrzej Sidor <andrzej.sidor@gmail.com>
2023-09-01 23:56:10 +0200

    First commit Java Spring

---
+++ src/main/java/library/movie/Movies/Movie.java
@@ -0,0 +1,17 @@
+package library.movie.Movies;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Movie {
+    private int id;
+    private String name;
+    private  int rating;
+
+
+
+}
---
+++ src/main/java/library/movie/Movies/MovieController.java
@@ -0,0 +1,32 @@
+package library.movie.Movies;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class MovieController {
+
+    @Autowired
+    MovieRepository movieRepository;
+
+    @GetMapping("/test")
+    public int test(){
+        return 1;
+    }
+    @GetMapping("/movies")
+    public List<Movie> getAll(){
+        return movieRepository.getAll();
+    }
+    @GetMapping("/movies/{id}")
+       public Movie getById (@PathVariable("id") int id)
+            {
+                return movieRepository.getbyId(id);
+            }
+
+
+}
